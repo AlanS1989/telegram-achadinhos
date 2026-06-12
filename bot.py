528dc9063dfa63e7385b7bc4875c14e3e72819ea
AlanS1989 <alansilva1612@gmail.com>
2026-06-12 10:08:51 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,35 @@
+import requests
+import pandas as pd
 
+TOKEN = "7757684344:AAHpaypxmjKTsDNRw9z--wvHwmPFoTu5QQI"
+
+CHAT_ID = "@achadinhosgolden01"
+
+sheet_id = "11CUjrM-7qra8TL84gleA5ns66UHR1FFIDOifWWxsoXY"
+
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+
+df = pd.read_csv(url)
+
+for _, row in df.iterrows():
+
+    mensagem = f"""
+🔥 ACHADINHO DO DIA
+
+🛍 Produto: {row['Produto']}
+
+💰 Preço: {row['Preço']}
+
+🛒 Comprar:
+{row['Link']}
+
+⚡ Oferta por tempo limitado
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": mensagem
+        }
+    )
